e057646763b5ae90998a8ce8274c905f8808676c
Tyler Machen <tylermachen@gmail.com>
2015-07-30 21:19:08 -0400

    complete deli counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  line_position = katz_deli.length + 1 
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{line_position} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    patron = katz_deli.shift
+    puts "Currently serving #{patron}."
+  end
+end
+
+def line(katz_deli)
+  output = ""
+  katz_deli.each_with_index { |patron, i| output << " #{i + 1}. #{patron}" }
+  puts katz_deli.length == 0 ? "The line is currently empty." : "The line is currently:#{output}"
+end
